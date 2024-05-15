2318751128cb46f91481d78054e64e592363f0bd
Amanda Shih <pandafulmanda@gmail.com>
2024-05-15 15:30:56 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.06f17fe42e7c99bb2d855d844814941c.js"
+  "./precache-manifest.d921fa9c75f6b3b7f39feeae14700383.js"
 );
 
 self.addEventListener('message', (event) => {
