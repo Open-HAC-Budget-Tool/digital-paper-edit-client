809b3d701d52cab240e888f725ec89973ed02c7e
Amanda Shih <pandafulmanda@gmail.com>
2024-05-15 21:15:57 -0500

    Updates

---
+++ precache-manifest.384fdd33774ce1297f90f306e685b1a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e342435d8ec1251ea792bef16acbbc97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e689176c919a3e79dc23",
+    "url": "./static/css/3.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "aaaf790487b38a910467",
+    "url": "./static/css/main.7565db1d.chunk.css"
+  },
+  {
+    "revision": "2f9b267a4e9b1ee51237",
+    "url": "./static/js/2.7baa2385.chunk.js"
+  },
+  {
+    "revision": "163e9998587691484c1ab309a3b23721",
+    "url": "./static/js/2.7baa2385.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e689176c919a3e79dc23",
+    "url": "./static/js/3.eb9339e1.chunk.js"
+  },
+  {
+    "revision": "26328d4bf17442650586f4961fef5065",
+    "url": "./static/js/3.eb9339e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd991aee0c16b57d2215",
+    "url": "./static/js/4.04539ab5.chunk.js"
+  },
+  {
+    "revision": "35eed31a716cb5dba27a",
+    "url": "./static/js/5.d3a594f0.chunk.js"
+  },
+  {
+    "revision": "a316d3dbf7723a3d0edb",
+    "url": "./static/js/6.cf080de4.chunk.js"
+  },
+  {
+    "revision": "aaaf790487b38a910467",
+    "url": "./static/js/main.1bac0120.chunk.js"
+  },
+  {
+    "revision": "8aa5a688e9285a82158c",
+    "url": "./static/js/runtime-main.565a6f21.js"
+  }
+]);
